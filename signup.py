98121efcde0d4539f9d0a9c0e80677b85296f64c
tanukhose <104750176+tanukhose@users.noreply.github.com>
2022-07-16 14:32:03 +0530

    Update and rename README.md to signup.py

---
+++ signup.py
@@ -0,0 +1,48 @@
+import json
+import os
+import re
+
+print("!WEL COME TO SIGN UP! ")
+file=os.path.exists("signup.json")
+user=input("what you want to login or signup:-")
+if user=="signup":
+        l=[]
+        name=input("enter the firstNAME:-")
+        sname=input("enter the lastNAME:-")
+        with open("signup.json","a")as f:
+            s=json.dump(l,f,indent=4)
+        t=open("signup.json","r")
+        n2=t.read()
+        # print(n2)
+        if name not in n2:
+            print("create a strong password mix of letter number and symbols")
+            def pasw():
+                p=input("enter the strong password:=")
+                if (re.search("[a-z A-Z]",p)and (re.search("[0-9]",p))and (re.search("[@#$&]",p))):
+                    if len(p)>=8:
+                        con_p=input("Enter the confirm password:")
+                        if p==con_p:
+                            print("coorect password")
+                            bod=(input("enter the birth date:-"))
+                            emali=input("enter the email ID:-")
+                            g=input("enter the gender male or female:-")
+                            print(name,"signup is succesfully")
+                            dic={'name':name,"Password":p,"DOB":bod,"Gender":g,"gmail":emali}
+                            with open("signup.json","r")as f:
+                                data=json.load(f)
+                            data.append(dic)
+                            with open("signup.json","w") as f:
+                                json.dump(data,f,indent=4)
+                        else:
+                            print("incrroct password")
+                            pasw()
+                    else:
+                        print("len is wrong")
+                        pasw()
+                else:
+                    print("check mix of letter number and symbols")
+                    pasw()
+            pasw()
+        else:
+            print("Name is already their")
+
